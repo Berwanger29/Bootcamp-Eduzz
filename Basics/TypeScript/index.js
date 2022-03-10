39faf4d6fd0de6d463f9ca447f322b9c47811702
Vinicius Berwanger <viniciusberwangers@gmail.com>
2022-03-10 09:33:31 -0400

    Notes about typescript finished

--- Basics/TypeScript/index.js
+++ Basics/TypeScript/index.js
@@ -2,39 +2,27 @@
 //     console.log(a + b)
 // }
 // soma(2, 3)
-
-
-
 //Interfaces
 // interface Animal{
 //     nome: string;
 //     tipo: 'terrestre' | 'aquatico';
 //     executarRugido(volumeSom: number): void
 // }
-
 // interface Felinos extends Animal{
 //     visaoNoturna:boolean;
 // }
-
-
 // const animal :Animal = {
 //     nome:'Elefante',
 //     tipo:"terrestre",
 //     executarRugido: (volumeSom)=>(`${volumeSom}db`)
 // }
 // animal.executarRugido(2)
-
 // const gato :Felinos = {
 //     nome:'Leao',
 //     tipo:"terrestre",
 //     visaoNoturna: true,
 //     executarRugido: (volumeSom) => (console.log(volumeSom)+'db')
 // }
-
-
-
-
-
 //TYPE
 // usando para definir o tipo de uma estrutura de dados, no exemplo abaixo
 // o tipo Domestico pode receber as interfaces de Canino ou Felino
@@ -43,17 +31,13 @@
 //     tipo:'terrestre' | 'aquatico';
 //     domestico: boolean;
 // }
-
 // interface Felino extends Animal{
 //     visaoNoturna:boolean;
 // }
-
 // interface Canino extends Animal{
 //     porte:'pequeno' |'medio'| 'grande'
 // }
-
 // type Domestico = Felino | Canino
-
 // const animal: Domestico = {
 //     domestico: true,
 //     nome:'Cachorro',
@@ -61,30 +45,22 @@
 //     tipo:"terrestre",
 //     visaoNoturna:false
 // }
-
 //INPUT HTML
 // let input = document.getElementById('input') as HTMLInputElement;
-
 // input.addEventListener('input',(event)=>{
 //     const i = event.currentTarget as HTMLInputElement;
 //     console.log(i.value)
 // })
-
-
-
 //GENERIC TYPES
 // function addAppendLista<NaoSei>(arr: NaoSei[], value:NaoSei){
 //     return arr.map(()=> value)
 // }
 // addAppendLista([1,2,3],1)
-
-
 // interface Usuario{
 //     id:string;
 //     email:string;
 //     cargo?:'funcionario'| 'gerente'| 'coordenador' | 'supervisor';
 // }
-
 // function redirect(usuario: Usuario){
 //     if(usuario.cargo === 'funcionario'){
 //         console.log('funcionario')
@@ -98,7 +74,4 @@
 //     email:'email',
 //     cargo:'gerente'
 // }
-
 // redirect(user)
-
-
---
+++ Basics/TypeScript/typings.d.ts
@@ -0,0 +1,13 @@
+// interface Estudante {
+//     nome: string;
+//     idade: number;
+// }
+
+// interface Estudante {
+//     serie: number;
+// }
+
+//todos as interfaces são somadas possibilitando acesso à ambas
+// const estudante: Estudante = {
+
+// }
