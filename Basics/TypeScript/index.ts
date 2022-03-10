7c9d7e4bb816c9f43d24ac00f10154f024a3545b
Vinicius Berwanger <viniciusberwangers@gmail.com>
2022-03-09 21:32:37 -0400

    Learning about typescript parameters and interfaces

---
+++ Basics/TypeScript/index.ts
@@ -0,0 +1,32 @@
+// function soma(a: number, b:number){
+//     console.log(a + b)
+// }
+// soma(2, 3)
+
+
+
+//Interfaces
+// interface Animal{
+//     nome: string;
+//     tipo: 'terrestre' | 'aquatico';
+//     executarRugido(volumeSom: number): void
+// }
+
+// interface Felinos extends Animal{
+//     visaoNoturna:boolean;
+// }
+
+
+// const animal :Animal = {
+//     nome:'Elefante',
+//     tipo:"terrestre",
+//     executarRugido: (volumeSom)=>(`${volumeSom}db`)
+// }
+// animal.executarRugido(2)
+
+// const gato :Felinos = {
+//     nome:'Leao',
+//     tipo:"terrestre",
+//     visaoNoturna: true,
+//     executarRugido: (volumeSom) => (console.log(volumeSom)+'db')
+// }
