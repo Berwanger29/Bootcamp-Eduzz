c40df6c9e31bf02cb65c31d555f8a05ee9c373d0
Vinicius Berwanger <viniciusberwangers@gmail.com>
2022-03-09 20:31:32 -0400

    Diferents methods of tests with mocha

--- Basics/Tests/math.js
+++ Basics/Tests/math.js
@@ -1,7 +1,12 @@
 class Math {
-    sum = function sum(a, b){
-        return a + b;
+    sum(a, b, callback){
+        setTimeout(()=>{
+            callback(a + b);
+        },2500)
+    }
+    multiply(a,b){
+        return a * b
     }
 }
 
-module.exports =Math;
+module.exports = Math;
--- Basics/Tests/test/math.spec.js
+++ Basics/Tests/test/math.spec.js
@@ -2,10 +2,24 @@ const assert = require('assert')
 const Math = require('../math.js')
 
 describe('Math class', function(){
-    it('Sum two numbers', function(){
+    //hooks
+    beforeEach(function(){
+        let value = 5;
+    })
+
+    it.skip('Sum two numbers', function(done){
+        const math = new Math();
+        this.timeout(3000)
+         
+        math.sum(5 ,5, value => {
+            assert.equal(value, 10);
+            done()
+        })
+    })
+
+    it.only('Multiply two numbers', function(){
         const math = new Math();
 
-        
-        assert.equal(math.sum(5,5), 10)
-    })
+        assert.equal(math.multiply(5,5), 25)
+    });
 })
